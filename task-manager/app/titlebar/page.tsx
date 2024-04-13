53c050c2aa385c68537b6528c937a8a0db6b8ed7
dhughneil <dhughneil@protonmail.com>
2024-04-12 19:06:00 -0500

    added design recommendations.

---
+++ task-manager/app/titlebar/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <div>Task Manager</div>
+    <button className='btn btn-border-warning'>Add</button>
+    <button type="button" className="btn btn-primary">Primary</button>
+    </>
+  )
+}
+
+export default page
