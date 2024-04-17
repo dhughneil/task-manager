43587991494aa271c73cfbb4e2316fee2bc11998
dhughneil <dhughneil@protonmail.com>
2024-04-16 21:12:33 -0500

    Styled active Link in the NavBar

--- task-manager/app/NavBar.tsx
+++ task-manager/app/NavBar.tsx
@@ -1,8 +1,14 @@
+'use client'
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { FaPlus, FaFilter, FaRegCheckSquare } from "react-icons/fa";
+import classNames from "classnames";
+
 
 const NavBar = () => {
+    const currentPath = usePathname();
+
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
@@ -12,14 +18,23 @@ const NavBar = () => {
         <li className="text-green-700">David's Task Manager</li>
         <li>
         <Link href='/add'>
-        <button className="flex items-center hover:text-red-600 transition-colors">
+        <button className={classNames({
+            'text-zinc-500': currentPath === '/add',
+            'text-zinc-900': currentPath !== '/add',
+            'flex items-center hover:text-red-600 transition-colors': true
+        })}>
         <span className="mr-2"><FaPlus /></span> Add
           </button>
           </Link>
+
         </li>
         <li>
         <Link href='/filter'>
-          <button className="flex items-center hover:text-red-600">
+        <button className={classNames({
+            'text-zinc-500': currentPath === '/filter',
+            'text-zinc-900': currentPath !== '/filter',
+            'flex items-center hover:text-red-600 transition-colors': true
+        })}>
           <span className="mr-2"><FaFilter /></span> Filter
           </button>
           </Link>
