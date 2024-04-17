f06f4bbc6d2604c33d2d76e6621e897df6577d01
dhughneil <dhughneil@protonmail.com>
2024-04-17 18:00:08 -0500

    Create Task Model

---
+++ prisma/migrations/20240417225031_create_task/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `Task` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `description` TEXT NOT NULL,
+    `completed` BOOLEAN NOT NULL DEFAULT false,
+    `dueDate` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
