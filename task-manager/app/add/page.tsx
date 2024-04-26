49468d62db72998b63296a9a0d48e044cb1357f2
dhughneil <dhughneil@protonmail.com>
2024-04-26 18:11:38 -0500

    Built the new task page

--- task-manager/app/add/page.tsx
+++ task-manager/app/add/page.tsx
@@ -1,10 +1,31 @@
-import React from "react";
-import { Button } from "@radix-ui/themes";
+"use client";
+import React, { useState } from "react";
+import { Button, TextField, TextArea } from "@radix-ui/themes";
+// import SimpleMDE from "react-simplemde-editor";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const page = () => {
+  const [dueDate, setDueDate] = useState(null);
+
   return (
     <>
-      <div>
+      <div className="max-w-xl space-y-3">
+        <TextField.Root placeholder="Task" />
+        <DatePicker
+          selected={dueDate}
+          onChange={(date) => setDueDate(date)}
+          placeholderText="Click to add a due date"
+        />
+        <TextArea className="flex" placeholder="Additional information…" />
+        
+        {/* <SimpleMDE
+          onChange={handleDescriptionChange}
+          options={{
+            placeholder: "Description...",
+          }}
+        /> */}
+        
         <Button>Add Task</Button>
       </div>
     </>
